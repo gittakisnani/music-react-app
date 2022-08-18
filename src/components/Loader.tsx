627b4d12de9479b5360ffac4a949c586fe46a184
Medwebdev <mtakisnani09@gmail.com>
2022-08-18 18:05:11 +0100

    Updates

--- src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="wrapper flex-1 bg-[#181818] flex items-center justify-center">
-      <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-    </div>
-  )
-}
-
-export default Loader
